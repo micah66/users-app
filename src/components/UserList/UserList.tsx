be256612159350c4c850254af468519a1041d291
micah66 <micahgordon66@gmail.com>
2023-07-30 00:30:13 +0300

    add edit and delete user functionality

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -2,16 +2,26 @@ import { List } from '@mui/material'
 import User, { TUser } from '../User/User'
 import { useLocalStorage } from '../../utils/hooks/useStorage'
 
-export default function UserList() {
-  const [users] = useLocalStorage<TUser[]>({ key: 'users' })
+export default function UserList({
+  onEdit,
+}: {
+  onEdit: (user: TUser) => void
+}) {
+  const [users, setUsers] = useLocalStorage<TUser[]>({ key: 'users' })
+
   return (
     <List>
       {users.map((user) => (
         <User
           key={user.id}
           username={user.username}
-          handleDelete={() => {}}
-          handleEdit={() => {}}
+          role={user.role}
+          handleDelete={() => {
+            setUsers((prevUsers) =>
+              prevUsers.filter((currentUser) => currentUser.id !== user.id),
+            )
+          }}
+          handleEdit={() => onEdit(user)}
         />
       ))}
     </List>
--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,12 +1,30 @@
-import { Typography } from '@mui/material'
+import { useState } from 'react'
+import { Dialog, Typography } from '@mui/material'
 
 import UserList from '../components/UserList/UserList'
+import EditUserForm from '../components/User/EditUserForm'
+import { TUser } from '../components/User/User'
 
 export default function UsersPage() {
+  const [user, setUser] = useState<TUser | null>(null)
+
+  const handleCloseDialog = () => setUser(null)
+
   return (
     <>
       <Typography variant="h3">Users</Typography>
-      <UserList />
+      <UserList onEdit={(user) => setUser(user)} />
+      <Dialog
+        open={!!user}
+        onClose={handleCloseDialog}
+        sx={{ '.MuiPaper-root': { width: '75%' } }}
+      >
+        <EditUserForm
+          user={user!}
+          onSave={handleCloseDialog}
+          onCancel={handleCloseDialog}
+        />
+      </Dialog>
     </>
   )
 }

62ad18dbc36826c8351dfff9506cb8dbff7fa63b
micah66 <35034217+micah66@users.noreply.github.com>
2023-07-30 09:48:32 +0300

    add basic authentication for login (#8)

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,21 +1,43 @@
-import { TextField, Button } from '@mui/material'
+import { useState } from 'react'
+import { TextField, Button, Typography } from '@mui/material'
 import { useForm } from 'react-hook-form'
-import { StyledForm } from './styles'
 
-type FormValues = {
-  username: string
-  email: string
-}
+import { StyledForm } from './styles'
+import { TUser } from '../User/User'
+import { useLocalStorage } from '../../utils/hooks/useStorage'
+import router from '../../routes'
+
+type FormValues = Pick<TUser, 'username' | 'email'>
 
 export default function LoginForm() {
+  const [users] = useLocalStorage<TUser[]>({ key: 'users', initValue: [] })
+  const [, setActiveUser] = useLocalStorage<TUser | null>({
+    key: 'activeUser',
+    initValue: null,
+  })
+  const [error, setError] = useState<string | null>(null)
   const { register, handleSubmit } = useForm<FormValues>()
 
-  const onSubmit = handleSubmit((data) => console.log('data', data))
+  const onSubmit = handleSubmit(async ({ username, email }) => {
+    const user = users.find((user) => user.username === username)
+
+    if (!user || user.email !== email) {
+      return setError('Invalid credentials')
+    }
+
+    await setActiveUser(user)
+    router.navigate({ to: '/' })
+  })
 
   return (
     <StyledForm onSubmit={onSubmit}>
       <TextField type="text" placeholder="Username" {...register('username')} />
       <TextField type="text" placeholder="Email" {...register('email')} />
+      {error && (
+        <Typography paragraph color="red">
+          {error}
+        </Typography>
+      )}
       <Button
         type="submit"
         variant="contained"

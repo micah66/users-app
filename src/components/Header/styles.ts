4e4914712a03e3b9bb8b4aad119f8def9718e312
micah66 <micahgordon66@gmail.com>
2023-07-30 11:15:00 +0300

    add logout button

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,7 @@
+import { styled } from '@mui/material/styles'
+import { Box } from '@mui/material'
+
+export const StyledContainer = styled(Box)(() => ({
+  display: 'flex',
+  justifyContent: 'flex-end',
+}))

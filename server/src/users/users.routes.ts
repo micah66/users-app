641ce7242dbef83f049dd54433294dbdeaf3cf4b
micah66 <micahgordon66@gmail.com>
2023-07-31 14:50:16 +0300

    get single user

--- server/src/users/users.routes.ts
+++ server/src/users/users.routes.ts
@@ -4,6 +4,7 @@ import * as UsersHandlers from './users.handlers'
 const router = Router()
 
 router.get('/', UsersHandlers.getAll)
+router.get('/:id', UsersHandlers.getById)
 router.post('/', UsersHandlers.createOne)
 
 export default router

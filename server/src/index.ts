47a1d72f12abfd78a44e0ae55a612bc5bc721886
micah66 <micahgordon66@gmail.com>
2023-07-31 17:33:07 +0300

    add authentication and authorization

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,7 @@
 import express, { Express, Request, Response, Router } from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
 import users from './users/users.routes'
 import login from './login/login.routes'
 
@@ -11,6 +14,12 @@ router.use('/users', users)
 router.use('/', login)
 
 app.use(express.json())
+app.use(cookieParser())
+app.use(
+  cors({
+    origin: ['http:localhost:5173'],
+  }),
+)
 
 app.get('/', (_req: Request, res: Response) => {
   res.status(200).send('Working')
--- server/src/users/users.model.ts
+++ server/src/users/users.model.ts
@@ -6,6 +6,7 @@ import { db } from '../db'
 export const User = z.object({
   username: z.string(),
   password: z.string(),
+  role: z.enum(['admin', 'user']),
 })
 
 export type User = z.infer<typeof User>

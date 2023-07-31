951ce1faec4d4dc678911262482af8f94427bd8a
micah66 <35034217+micah66@users.noreply.github.com>
2023-07-31 14:51:40 +0300

    get single user (#19)

--- server/src/users/users.handlers.ts
+++ server/src/users/users.handlers.ts
@@ -1,10 +1,21 @@
 import { RequestHandler, Response, Request } from 'express'
 import { User, UserWithId, Users } from './users.model'
-import { InsertOneResult } from 'mongodb'
+import { InsertOneResult, ObjectId } from 'mongodb'
 
 export const getAll: RequestHandler = async (req, res: Response<User[]>) => {
   const result = await Users.find()
   res.json(await result.toArray())
+}
+
+export const getById = async (
+  req: Request<{ id: string }, UserWithId>,
+  res: Response<UserWithId>,
+) => {
+  const result = await Users.findOne({ _id: new ObjectId(req.params.id) })
+  if (!result) {
+    throw new Error(`User with id ${req.params.id} does not exist`)
+  }
+  res.json(result)
 }
 
 export const createOne: RequestHandler = async (
